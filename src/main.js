58a9c7b90940d8a3133d6c7894139fc02ebbb97b
Min <79357141+mjtwo20@users.noreply.github.com>
2025-10-28 17:27:53 -0700

    part5 create an Event Listener for read-more

--- src/main.js
+++ src/main.js
@@ -134,6 +134,11 @@ async function displayCardsDynamically() {
       // Set the image source
       newcard.querySelector(".card-image").src = `./images/${hike.code}.jpg`;
 
+      // Add the link with the document ID
+      newcard.querySelector(
+        ".read-more"
+      ).href = `eachHike.html?docID=${doc.id}`;
+
       // Attach the new card to the container
       document.getElementById("hikes-go-here").appendChild(newcard);
     });
